f1f4efdda9ac5766d83e2f72040d3048440e10b3
Nyx404 <cherrycoda5@gmail.com>
2025-05-20 06:35:16 +0300

    Update Problem-02.cpp

---
+++ Algo-Level-2/problems_1-5/Problem-02.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+string ReadName() {
+	string name;
+
+	cout << "Enter your Name: \n";
+	cin >> name;
+
+	return name;
+}
+
+void PrintName(string name) {
+	cout << "Hello " << name << endl;
+}
+int main()
+{
+	PrintName(ReadName());
+
+	return 0;
+
+}
+
+

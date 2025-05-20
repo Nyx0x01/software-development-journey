cf96d04efcf73910649893ffd3dfccdc09465466
Nyx404 <cherrycoda5@gmail.com>
2025-05-20 06:24:26 +0300

    Update Problem-01.cpp

---
+++ Algo-Level-2/problems_1-5/Problem-01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+//Problem 01
+void PrintName() {
+	cout << "Hello i'm Nyx a software engineer <3 \n";
+}
+int main()
+{
+	PrintName();
+	return 0;
+
+}
+
+

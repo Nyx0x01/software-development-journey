ccbac4fec0a240d51d3503923acd54697833e9f9
Nyx404 <cherrycoda5@gmail.com>
2025-05-23 20:54:34 +0300

    Update 13-MaxOfThree.cpp
    Updated the MaxOf3Numbers function

--- Algo-Level-2/problems_11-15/13-MaxOfThree.cpp
+++ Algo-Level-2/problems_11-15/13-MaxOfThree.cpp
@@ -17,20 +17,19 @@ int ReadNumber() {
 int MaxOf3Numbers(int num1, int num2, int num3) {
 	
 	if (num1 > num2) {
-		if (num1 > num3) {
+		if (num1 > num3) 
 			return num1;
-		}
-		else {
+		
+		else 
 			return num3;
-		}
-	}
-	else if (num2 > num3) {
-		return num2;
+		
 	}
 	else {
+		if (num2 > num3) 
+		return num2;
+	
+	else 
 		return num3;
-	}
-
 }
 
 void PrintMax(int max) {

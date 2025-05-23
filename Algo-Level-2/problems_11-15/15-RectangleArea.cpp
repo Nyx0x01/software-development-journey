5eb0b4854cb9317c3919d6280e3fae72f4209361
Nyx404 <cherrycoda5@gmail.com>
2025-05-23 22:21:26 +0300

    Update and rename 15-RectangleArea to 15-RectangleArea.cpp
    Calc Rec Area

---
+++ Algo-Level-2/problems_11-15/15-RectangleArea.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+void ReadNumbers(float& num1, float& num2) {
+	cout << "Enter first number: \n";
+	cin >> num1;
+	cout << "Enter second number: \n";
+	cin >> num2;
+}
+
+float CalculateArea(float height, float width) {
+	
+	return height * width;
+}
+
+void PrintArea(float Area) {
+	cout << "Area of your rectangle = " << Area << endl;
+}
+int main()
+{
+
+	float height, width;
+	ReadNumbers(height, width);
+	PrintArea(CalculateArea(height, width));
+
+	return 0;
+}
+
+

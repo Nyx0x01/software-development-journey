30bb689843226f3ce50c6d927bba5bec506de2ea
Nyx404 <cherrycoda5@gmail.com>
2025-05-26 02:03:19 +0300

    Create 16-CalculateAreaBasedOnSideAndDiagonalLine.cpp

---
+++ Algo-Level-2/Problems-16-20/16-CalculateAreaBasedOnSideAndDiagonalLine.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+void ReadNumbers(float &A, float &B) {
+
+	cout << "Enter first number: \n";
+	cin >> A;
+
+	cout << "Enter second number: \n";
+	cin >> B;
+}
+
+float CalculateRectangleArea(float A, float B) {
+	float Area =  A * (sqrt(pow(B, 2) - pow(A, 2)));
+	return Area;
+}
+
+void PrintArea(float Area) {
+	cout << "Your rectangle's area = " << Area << endl;
+}
+int main()
+{
+	float width, diagonal;
+	ReadNumbers(width, diagonal);
+	PrintArea(CalculateRectangleArea(width, diagonal));
+	return 0;
+}
+
+
+

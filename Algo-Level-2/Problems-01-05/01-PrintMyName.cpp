2f4c84b9eeea1b2a23e9f72a42f2ba0638fa3b9b
Nyx404 <cherrycoda5@gmail.com>
2025-05-26 01:15:54 +0300

    Create 01-PrintMyName.cpp

---
+++ Algo-Level-2/Problems-01-05/01-PrintMyName.cpp
@@ -0,0 +1,39 @@
+/*
+--------------------------------------------------
+🧠 Problem: Print my name on screen
+📁 Level: 02 - Algorithms
+🎯 Topic: Basic I/O
+👩‍💻 Author: Nyx0x01
+--------------------------------------------------
+
+📌 Description:
+Write a program that prints your name
+
+🧮 Input:
+Name
+
+📤 Output:
+Print name
+
+🔁 Example:
+Output: Hello My name is Nyx
+
+
+🧩 Approach:
+- A simple function to print the name
+
+--------------------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+void PrintName(string name) {
+	cout << "Hello i'm " << name << " a software engineer <3 \n";
+}
+int main()
+{
+	PrintName("Nyx");
+	return 0;
+
+}

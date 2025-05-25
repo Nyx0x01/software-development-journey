eaed1d010703788ae712965c7daf9bd6ce32eb48
Nyx404 <cherrycoda5@gmail.com>
2025-05-26 01:52:43 +0300

    Create 09-Sum3Numbers.cpp

---
+++ Algo-Level-2/Problems-06-10/09-Sum3Numbers.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+struct stNumbers {
+	int num1, num2, num3;
+};
+
+stNumbers ReadNumbers() {
+	stNumbers n;
+
+	cout << "Enter first number: \n";
+	cin >> n.num1;
+
+	cout << "Enter second number: \n";
+	cin >> n.num2;
+
+	cout << "Enter third number: \n";
+	cin >> n.num3;
+
+	return n;
+}
+
+int SumNumbers(stNumbers n) {
+	int sum = 0;
+
+	sum = n.num1 + n.num2 + n.num3;
+
+	return sum;
+}
+
+void PrintResult(int sum) {
+	cout << "Summation of entered numbers: " << sum << endl;
+}
+int main()
+{
+	PrintResult(SumNumbers(ReadNumbers()));
+	return 0;
+
+}

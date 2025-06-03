71157efad587af49d53383314c8aed50c5a37b3c
Nyx404 <cherrycoda5@gmail.com>
2025-06-03 23:40:15 +0300

    Update 46-PrintLetters.cpp

--- Algo-Level-2/Problems-46-50/46-PrintLetters.cpp
+++ Algo-Level-2/Problems-46-50/46-PrintLetters.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
 
+using namespace std;
+
+void PrintLettersAtoZ() {
+	for (int i = 65; i <= 90; i++) {
+		cout << (char)i << endl;
+	}
+}
+
+int main()
+{
+	PrintLettersAtoZ();
+
+	return 0;
+}
